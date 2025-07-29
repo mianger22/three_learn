e631b802b864b8d63812afc8829cd4b874eba4ae
mianger22 <test666777888999@yandex.ru>
2025-07-29 08:22:53 +0300

    Добавил туман и переместил да повернул куб, чтобы видеть его с ребра

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import * as THREE from 'three';
 // Настройка проекта
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 'lightpink' );
+// Добавлен туман, на будущее
+scene.fog = new THREE.Fog( 0x59472b, 1, 1000 );
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 
     1000 );
@@ -14,16 +16,15 @@ renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 // Создание модели
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 'red' } );
+const geometry = new THREE.BoxGeometry( 2, 3, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 'brown' } );
 const cube = new THREE.Mesh( geometry, material );
+cube.position.x = 3;
 
 // Добавление на сцену
 scene.add( cube );
 
 // Анимирование модели
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     renderer.render( scene, camera );
 };
