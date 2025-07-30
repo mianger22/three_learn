be7fa9c0be50100e276fc5009d80f245e230ab0a
mianger22 <test666777888999@yandex.ru>
2025-07-30 20:30:12 +0300

    настроил туман, добавил серости миру

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ import * as THREE from 'three';
 // ------------------- Настройка проекта -------------------------------
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color('white');
-// Добавлен туман, на будущее
-scene.fog = new THREE.Fog(0x59472b, 1, 1000);
+scene.background = new THREE.Color('gray');
+scene.fog = new THREE.FogExp2(0x59472b, 0.1, 1000);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 10.5;
